8bd11c09a1f17c97ae2321eab413598883e910a2
gil-miranda <gilsmneto@gmail.com>
2022-05-23 18:07:41 -0300

    Data Automation feature

--- src/download-data.py
+++ src/download-data.py
@@ -1,7 +1,29 @@
-# Automation To be done
+## This is a work in progress
+import yaml
+import requests
+import os
 
-# import yaml
+with open(r'config.yml') as file:
+    CONFIG = yaml.safe_load(file)
 
-# with open(r'../config.yml') as file:
-#     CONFIG = yaml.safe_load(file)
+def check_folders():
+    folder_exists = os.path.isdir(CONFIG['datasets']['folder'])
+    if not folder_exists:
+        create_folders()
+    return folder_exists
 
+def create_folders():
+    os.mkdir('data')
+    os.mkdir(CONFIG['datasets']['folder'])
+    return None
+
+def download_data(section='section-01'):
+    for ds in CONFIG['datasets'][section]:
+        for _,f in CONFIG['datasets'][section][ds].items():
+            response = requests.get(f)
+            file_name = f.split('/')[-1]
+            open(f"{CONFIG['datasets']['folder']}/{file_name}", 'wb').write(response.content)
+
+if __name__ == '__main__':
+    check_folders()
+    download_data()
